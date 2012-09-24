77fcd5de8a19fb60a7b1bd3ab053010e294dc6e5
KOBAYASHI Daisuke <daisuke.koba@gmail.com>
2012-09-24 23:54:48 +0900

    Update version of GCC

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 # ----------------------------------------------------------------------------
 
-gcc_version=4.7.1
+gcc_version=4.7.2
 binutils_version=2.22
 mingww64_version=2.0.6
 gmp_version=5.0.5
